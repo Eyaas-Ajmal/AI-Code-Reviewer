7a555cceeceeff6d014e289fe88466bc5f59a635
Eyaas-Ajmal <162302716+Eyaas-Ajmal@users.noreply.github.com>
2025-06-22 19:54:05 +0530

    open ai config set up !

--- server/server.js
+++ server/server.js
@@ -14,10 +14,6 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
-
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
